43d7e46bdbb790173c7caf6d885e2d63274b5adf
emiledebruycker <emiledebruyckere@gmail.com>
2020-09-02 12:35:09 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+prompt ("console kinderkijk voer commando in")
+prompt ("video id");
+console.log ("laden video");
+console.log ("please wait")
+setTimeout(() => {console.log ("laden voltooid"); console.log ("video start seconden"); }, 10000);
